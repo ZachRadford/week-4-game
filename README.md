ae7e3885cb2bfd5fac805922507cb5af19a6cd21
ZachRadford <zachradford13@gmail.com>
2017-05-16 15:20:29 -0700

    Upload

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # week-4-game
+https://zachradford.github.io/week-4-game/
+
+https://github.com/ZachRadford/week-4-game
---
+++ assets/javascript/game.js
@@ -0,0 +1,94 @@
+  
+
+
+  var target
+
+  var counter
+
+  var numberOptions
+
+  var fuck
+
+  $("#playAgain").on("click", function(){
+    setupGame();
+  })
+
+setupGame();
+
+  function setupGame(){
+    target = Math.floor(Math.random() * 100) + 1;;
+    $("#guessWhat").text(target);
+    counter = 0;
+    numOptions();
+    imgDraw();
+    fuck = $(".crystal-image")
+    addclick();
+    $("#playAgain").css("display", "none")
+  }
+
+
+function numOptions(){
+  numberOptions = [1, 5, 3, 7, 10, 12].sort(function(a, b){
+    console.log(a, b);
+    return 0.5 - Math.random()
+  }); 
+}
+
+
+ function imgDraw(){
+    for (var i = 0; i < numberOptions.length; i++) {
+
+   
+      var imageCrystal = $("<img>");
+
+
+      imageCrystal.addClass("crystal-image");
+
+      
+      imageCrystal.attr("src", "assets/images/3O8AMde.jpg");
+
+     
+      imageCrystal.attr("data-crystalvalue", numberOptions[i]);
+
+    
+      $("#crystals").append(imageCrystal);
+    }
+  }
+
+
+  function addclick(){
+
+      fuck.on("click", function() {
+
+      
+      var crystalValue = ($(this).attr("data-crystalvalue"));
+      crystalValue = parseInt(crystalValue);
+
+      counter += crystalValue;
+
+      console.log("New score: " + counter);
+
+      if (counter === target) {
+        console.log("You win! Go f*ck yourself.");
+        playAgain();
+        return;
+        
+      }
+
+      else if (counter >= target) {
+        console.log("You lose, nobody likes you, you will have low resale value on your home, low tire pressure, and cluttered drawers.")        
+        playAgain();
+        return;
+      }
+
+    });
+  }
+
+
+    function playAgain(){
+        $("#crystals").empty();
+        $("#playAgain")
+          .css("display", "block")
+
+
+    }
